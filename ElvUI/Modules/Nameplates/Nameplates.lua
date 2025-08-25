a729f164ff8edf4e8c1a7159702984041da288be
Crumdidlyumshis <stschleyer89@gmail.com>
2025-08-24 22:12:30 -0500

    Revert "Nameplates: Keep the scale of nameplates same as E.uiscale"
    This reverts commit 835351051158d02bd57998ca9c47ed7d768ce858.

--- ElvUI/Modules/Nameplates/Nameplates.lua
+++ ElvUI/Modules/Nameplates/Nameplates.lua
@@ -578,7 +578,6 @@ function NP:OnCreated(frame)
 	unitFrame:Hide()
 	unitFrame:SetAllPoints()
 	unitFrame:SetScript("OnEvent", self.OnEvent)
-	unitFrame:SetScale(E.uiscale)
 	unitFrame.plateID = plateID
 
 	unitFrame.Health = self:Construct_HealthBar(unitFrame)
